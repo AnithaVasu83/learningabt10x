00c045b60793419adbe95f191126c127d4080644
AnithaVasu83 <anithavasu83@gmail.com>
2025-01-20 15:21:40 +0530

    final

---
+++ src/Lab001_main_comments.java
@@ -0,0 +1,15 @@
+public class Lab001_main_comments {
+    public static void main (String[] args) {
+        //comments are used for understanding the program better
+        /**
+         * for multiple lines
+         * use this type of comment
+         * symbol
+         */
+        //main method is the entry point of the program
+        //to run the application JVM looks for main method.
+
+        System.out.println("Can have multiple main method but only with the public static void main");
+    }
+}
+
