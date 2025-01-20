2cd060a3a4d6798df340484986f43f4845e3e2a6
AnithaVasu83 <anithavasu83@gmail.com>
2025-01-20 19:45:35 +0530

    latest1

---
+++ src/Lab006_arithmetic_operator.java
@@ -0,0 +1,22 @@
+public class Lab006_arithmetic_operator {
+    public static void main(String[] args)
+    {
+        int a= 10;
+        int b=3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        float c=10.31f;
+        int d=3;
+        System.out.println(c + d);
+        System.out.println(c - d);
+        System.out.println(c * d);
+        System.out.println(c / d);
+        System.out.println(c % d);
+
+    }
+}
